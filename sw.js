87483ea62710d86d5974d09a0175bf4ce49810eb
mannanimuhsin-sys <mannanimuhsin@gmail.com>
2026-03-14 09:42:34 +0530

    update data

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('madrasa-store').then((cache) => cache.addAll([
+      './index.html',
+      './style.css', // നിങ്ങളുടെ സി.എസ്.എസ് ഫയൽ പേര്
+      './script.js'  // നിങ്ങളുടെ ജാവാസ്ക്രിപ്റ്റ് ഫയൽ പേര്
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
